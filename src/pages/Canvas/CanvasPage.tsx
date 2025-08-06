ff6bce466f91c72a7311da0a254cb5b87ba701f3
sablastBF <filip.barbaric99@gmail.com>
2025-08-06 18:24:09 +0200

    Adding icons and buttons outline without functionality.

--- src/pages/Canvas/CanvasPage.tsx
+++ src/pages/Canvas/CanvasPage.tsx
@@ -1,38 +1,64 @@
 import { Viewport } from "pixi-viewport";
 import { Application, Graphics } from "pixi.js";
 import { Button } from "primereact/button";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { getPixiApp, getViewPort } from "./services/pixi-app";
+import { Canvas } from "./services/pixi-app";
+import {
+  Circle,
+  Eraser,
+  MousePointer2,
+  MoveLeft,
+  PenLine,
+  Square,
+  ZoomIn,
+  ZoomOut,
+} from "lucide-react";
+import { ZoomedEvent } from "pixi-viewport/dist/types";
 
 function CanvasPage() {
   const divContainer = useRef<HTMLDivElement>(null);
-
+  const [colorHEX, setColorHEX] = useState("#6466f1"); // Added '#' for proper HEX format
+  const [zoomed, setZoomed] = useState(1);
   async function SetUpPixi() {
-    // if (app.current) return;
-    const app = await getPixiApp();
-    console.log("Createing new app. ");
-    if (divContainer.current   ) {
-      console.log(app);
-      console.log(app.canvas);
+    const app = await Canvas.getPixiApp();
+    if (divContainer.current) {
       divContainer.current.appendChild(app.canvas);
     }
+
+    const viewport = Canvas.getViewport();
+    viewport.on("zoomed", (e: ZoomedEvent) => {
+      setZoomed(viewport?.scale.x);
+    });
   }
   SetUpPixi();
-  
-  function AddCircle(){
-    const viewport = getViewPort();
-    viewport.addChild(new Graphics().circle(10, 54, 10).fill("red"));
-  }
 
   return (
-    <>
-    <div>
-      <Button onClick={()=>AddCircle()}></Button>
-      <Link className="h-full w-full" to="/page-two">Go to page one</Link>AddCircle
-      <div ref={divContainer} className="h-screen w-screen" />
+    <div className="relative h-screen w-screen">
+      <div ref={divContainer} className="absolute inset-0" />
+
+      <div className="absolute bottom-0 left-0 right-0 flex justify-center pb-2">
+        <div className="flex justify-center items-center gap-4  bg-white/10 backdrop-blur-sm rounded-lg p-2  min-w-min">
+          <MousePointer2 color="white" size={32} />
+          <PenLine color="white" size={32} className="transform" />
+          <Eraser color="white" size={32} />
+          <Square color="white" size={32} />
+          <Circle color="white" size={32} />
+        </div>
+      </div>
+      <div className="absolute left-0  p-4 flex justify-center">
+        <div className="rounded-2xl p-2 bg-white/10 backdrop-blur-sm hover:outline-2 outline-amber-300 shadow-2xl transition-all duration-100">
+          <MoveLeft color="white" size={30} />
+        </div>
+      </div>
+      <div className="absolute right-0 bottom-0 p-2">
+        <div className="flex justify-center items-center rounded-2xl p-2 gap-2 bg-white/10 backdrop-blur-sm text-gray-300 text-xl">
+              <ZoomIn size={25}/>
+            {(zoomed * 100).toFixed(1)}%
+              <ZoomOut size={25}/>
+        </div>
+      </div>
     </div>
-    </>
   );
 }
 
--- src/pages/Canvas/services/pixi-app.ts
+++ src/pages/Canvas/services/pixi-app.ts
@@ -1,30 +1,79 @@
 import { Viewport } from "pixi-viewport";
-import { Application } from "pixi.js";
+import { ZoomedEvent } from "pixi-viewport/dist/types";
+import { Application, FederatedMouseEvent, Graphics } from "pixi.js";
 
-let appInstance: Application | null = null;
-let viewport: Viewport | null = null;
+export namespace Canvas {
+  let appInstance: Application | null = null;
+  let viewport: Viewport | null = null;
 
-export async function getPixiApp() {
-  if (appInstance) return appInstance;
-  console.log("Createing new app. ");
-  appInstance = new Application();
-  await appInstance.init({ background: "gray", resizeTo: window });
-  viewport = new Viewport({
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerHeight,
-    worldWidth: 1024,
-    worldHeight: 1024,
-    events: appInstance.renderer.events,
-  });
+  export async function getPixiApp() {
+    if (appInstance) return appInstance;
+    console.log("Createing new app. ");
+    appInstance = new Application();
+    await appInstance.init({ background: "black", resizeTo: window });
+    viewport = new Viewport({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+      worldWidth: 1024,
+      worldHeight: 1024,
+      events: appInstance.renderer.events,
+    });
 
-  appInstance.stage.addChild(viewport);
-  viewport.drag({ mouseButtons: "middle" }).pinch().wheel();
-  return appInstance;
-}
+    appInstance.stage.addChild(viewport);
+    viewport.drag({ mouseButtons: "middle" }).pinch().wheel();
+    viewport.on("mousedown", (e) => {
+      startToDraw(e);
+    })
+    .on("mousemove", (e) => {
+      drawLine(e);
+    })
+    .on("mouseup", (e) => {
+      stopDrawing(e);
+    }).on("mouseout", (e) => {
+      stopDrawing(e);
+    });
 
-export function getViewPort() {
-  if (viewport) {
-    return viewport;
+    return appInstance;
   }
-  throw Error("");
+
+  export function getViewport() {
+    if (viewport) {
+      return viewport;
+    }
+    throw Error("");
+  }
+
+  let graph: Graphics;
+  let drawing = false;
+  let count = 0;
+
+  function startToDraw(e: FederatedMouseEvent) {
+    if (drawing) return;
+    drawing = true;
+    const viewport = getViewport();
+    const worldPos = viewport.toWorld(e.global);
+    graph =  new Graphics();
+    graph.moveTo(worldPos.x, worldPos.y);
+    graph.moveTo(worldPos.x, worldPos.y);
+    graph.stroke({ width: 10, color: "white", cap:"round" });
+    graph.tint = "yellow";
+    viewport.addChild(graph);
+    count = 0;
+  }
+
+  function drawLine(e: FederatedMouseEvent) {
+    if (!drawing) return;
+    if (graph === undefined) return;
+    const viewport = getViewport();
+    const worldPos = viewport.toWorld(e.global);
+    graph.lineTo(worldPos.x, worldPos.y);
+    graph.stroke({ width: 10, color: "white", cap:"round" });
+    count +=1;
+  }
+
+  function stopDrawing(e: FederatedMouseEvent){
+    if (!drawing) return;
+    drawing = false;
+    console.log(count);
+  }
 }
