d72cab9012afb2ec71e1a33a8bc9c1357d199724
sablastBF <filip.barbaric99@gmail.com>
2025-08-08 16:49:32 +0200

    commit

--- src/pages/Canvas/components/misc/DropdownToolSelector/DropdownToolSelector.tsx
+++ src/pages/Canvas/components/misc/DropdownToolSelector/DropdownToolSelector.tsx
@@ -4,39 +4,49 @@ import "./DropdownToolSelector.css";
 import { useState } from "react";
 import useCanvasStore from "../../../data/CanvasStore";
 
-const iconOptionTemplate = (option: IconOption) => {
-  return <div className="flex items-center">{option.icon}</div>;
-};
-
-const selectedIconTemplate = (option: IconOption) => {
-  if (option) {
-    return <div className="flex items-center">{option.icon}</div>;
-  }
-  return <span></span>;
-};
-
 export type DropdownToolSelectorParams = {
   dropDownOptions: IconOption[];
 };
 
 function DropdownToolSelector({ dropDownOptions }: DropdownToolSelectorParams) {
-  const [selectedDropDownOptions, setSelectedDropDownOptions] = useState(dropDownOptions[0]);
+  const [selectedDropDownOptions, setSelectedDropDownOptions] = useState(
+    dropDownOptions[0]
+  );
   const setActiveTool = useCanvasStore((state) => state.setActiveTool);
+  const iconOptionTemplate = (option: IconOption) => {
+    return <div className="flex items-center cursor-pointer">{option.icon}</div>;
+  };
+
+  const selectedIconTemplate = (option: IconOption) => {
+    if (option) {
+      return (
+        <div
+          className="flex items-center hover:cursor-pointer"
+          onClick={() => {
+            setActiveTool(selectedDropDownOptions.action);
+          }}
+        >
+          {option.icon}
+        </div>
+      );
+    }
+    return <span></span>;
+  };
+
   return (
     <>
       <Dropdown
+        className="cursor-pointer"
         value={selectedDropDownOptions}
         valueTemplate={selectedIconTemplate}
         options={dropDownOptions}
         itemTemplate={iconOptionTemplate}
         optionLabel="name"
         dataKey="action"
-        onClick={(e) => {
-          setActiveTool(selectedDropDownOptions.action);
-        }}
         onChange={(e) => {
           setSelectedDropDownOptions(e.value);
           setActiveTool((e.value as IconOption).action);
+          debugger;
         }}
       />
     </>
