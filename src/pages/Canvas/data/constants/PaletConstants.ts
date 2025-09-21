83fca5f578d52f2844293fea20cedfb2872ee9d2
sablastBF <filip.barbaric99@gmail.com>
2025-09-21 09:56:18 +0200

    Litele better circle cursor.

--- src/pages/Canvas/data/constants/PaletConstants.ts
+++ src/pages/Canvas/data/constants/PaletConstants.ts
@@ -48,4 +48,5 @@ export const PaletContainer = [
   // 
   { id: "bgt-1", light: "rgb(123, 123, 123)", dark: "rgb(123, 123, 123)" },
   { id: "c-1", light: "rgb(0, 0, 0)", dark: "rgb(255, 255, 255)" },
+  { id: "c-2", light: "rgb(125, 125, 125)", dark: "rgb(125, 125, 125)" },
 ];
--- src/pages/Canvas/features/cursor/Circle.ts
+++ src/pages/Canvas/features/cursor/Circle.ts
@@ -6,23 +6,14 @@ import { Continuum_CanvasCursor } from "./CursorManager";
 
 export class CircleCursor {
   static draw() {
-    const eraseMethod = useEraseStore.getState().eraseMethod;
     const zoom = useCanvasStore.getState().zoome;
-    if (eraseMethod === "soft") {
-      const radius =
-        zoom * ThicknesPalet.getThicknes(useEraseStore.getState().thicknesId);
-      Continuum_CanvasCursor.cursor.clear();
-      Continuum_CanvasCursor.cursor
-        .circle(0, 0, radius)
-        .fill({ color: CanvasPalet.getColor("c-1") })
-        .stroke({ width: 1, color: CanvasPalet.getColor("c-1") });
-    } else if (eraseMethod === "strong") {
-      const radius =
-        zoom * ThicknesPalet.getThicknes(useEraseStore.getState().thicknesId);
-      Continuum_CanvasCursor.cursor.clear();
-      Continuum_CanvasCursor.cursor
-        .circle(0, 0, radius)
-        .fill({ color: CanvasPalet.getColor("c-1") });
-    }
+
+    const radius =
+      zoom * ThicknesPalet.getThicknes(useEraseStore.getState().thicknesId);
+    Continuum_CanvasCursor.cursor.clear();
+    Continuum_CanvasCursor.cursor
+      .circle(0, 0, radius)
+      .fill({ color: CanvasPalet.getColor("c-2"), alpha: 0.5 })
+      .stroke({ width: 1, color: CanvasPalet.getColor("c-2") });
   }
 }
