362d3c390c77df7ee6627c3b6a0a6bd7105279b8
sablastBF <filip.barbaric99@gmail.com>
2025-10-28 21:41:32 +0100

    jesus christ i am happy for the first time.

--- src/pages/Canvas/features/input/IdleState.ts
+++ src/pages/Canvas/features/input/IdleState.ts
@@ -1,15 +1,19 @@
 import { Continuum_Canvas } from "../CanvasApp";
 import { Continuum_ToolManager } from "../tools/ToolManager";
 import { InputBinding } from "./InputBiding";
-import { MouseButton } from "./InputState";
+import { MouseButton, PointerType } from "./InputState";
 
 export const idleBidings: InputBinding[] = [
   // DRAWING
   {
     pointerDown: true,
     action: (e) => {
-      Continuum_Canvas.inputStateManager.SwitchState("DARWING");
-        Continuum_ToolManager.currentTool!.startDrawing!(e);
+      if (e.pointerType === PointerType.MOUSE) {
+        if (e.mouseButtons & 1) {
+          Continuum_Canvas.inputStateManager.SwitchState("DARWING");
+          Continuum_ToolManager.currentTool!.startDrawing!(e);
+        }
+      }
     },
     appState: ["IDLE"],
   },
--- src/pages/Canvas/features/service/Viewport.ts
+++ src/pages/Canvas/features/service/Viewport.ts
@@ -1,5 +1,6 @@
 import { Viewport } from "pixi-viewport";
 import { Continuum_Canvas } from "../CanvasApp";
+import { Continuum_ResizeService } from "./Resize";
 
 export namespace Continuum_CanvasViewport {
   export let viewport: Viewport | null = null;
@@ -12,5 +13,16 @@ export namespace Continuum_CanvasViewport {
       events: Continuum_Canvas.appInstance.renderer.events,
       passiveWheel: true,
     });
+    Continuum_CanvasViewport.viewport
+      .drag({ mouseButtons: "middle" })
+      .pinch({
+        noDrag: true,
+      })
+      .wheel();
+    Continuum_CanvasViewport.viewport
+      .on("zoomed", (e) => {
+        Continuum_ResizeService.viewportZoom(e);
+      })
+      .on("moved", () => {});
   }
 }
